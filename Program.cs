4674ad534169067bb3e8bf679c1ee213cae6d3ae
Boricua-Coder <lesther312@gmail.com>
2025-06-22 22:13:00 -0400

    Add initial project files and configuration for MachineKeyGenerator

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Security.Cryptography;
+
+Console.WriteLine("<machineKey");
+Console.WriteLine($"  validationKey=\"{GenerateKey(128)}\"");
+Console.WriteLine($"  decryptionKey=\"{GenerateKey(64)}\"");
+Console.WriteLine("  validation=\"SHA1\"");
+Console.WriteLine("  decryption=\"AES\" />");
+
+static string GenerateKey(int length) {
+    byte[] buffer = new byte[length / 2];
+    using (var rng = RandomNumberGenerator.Create()) {
+        rng.GetBytes(buffer);
+    }
+    return BitConverter.ToString(buffer).Replace("-", "");
+}
